75fc347fb75434a3b86937d1d37076261e29f717
Gavrilov Miroslav <gavrilov.miroslav@gmail.com>
2018-10-24 02:01:35 -0700

    Added intersects problem

---
+++ intersects/src/main/scala/Main.scala
@@ -0,0 +1,8 @@
+package gm.interviews.intercepts
+
+object Main {
+
+  def main(args: Array[String]): Unit =
+    println("Hello World!")
+}
+
---
+++ intersects/src/test/scala/EmptySpec.scala
@@ -0,0 +1,9 @@
+package gm.interviews.intercepts
+
+import org.scalatest._
+
+class EmptySpec extends FlatSpec with Matchers {
+
+  // Add tests here
+}
+
