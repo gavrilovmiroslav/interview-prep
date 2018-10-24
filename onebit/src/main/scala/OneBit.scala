a2e42a7e224c26f0f88f7a3a5362026f7e5742d8
Gavrilov Miroslav <gavrilov.miroslav@gmail.com>
2018-10-24 02:28:49 -0700

    Added the one bit problem

---
+++ onebit/src/main/scala/OneBit.scala
@@ -0,0 +1,8 @@
+package gm.interviews.onebit
+
+object Main {
+
+  def main(args: Array[String]): Unit =
+    println("Hello World!")
+}
+
---
+++ onebit/src/test/scala/OneBitSpec.scala
@@ -0,0 +1,9 @@
+package gm.interviews.onebit
+
+import org.scalatest._
+
+class EmptySpec extends FlatSpec with Matchers {
+
+  // Add tests here
+}
+
